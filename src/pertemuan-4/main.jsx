449c1d6c61d23f498f6f09506cb29725605936cb
bungales <bunga24si@mahasiswa.pcr.ac.id>
2026-04-09 16:06:03 +0700

    lanjutan P4  Responsive & Grid Design

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -1,14 +1,15 @@
 import { createRoot } from "react-dom/client";
-import './tailwind.css';
-import FrameworkList from "./FrameworkList";  
-import FrameworkSearchFilter from "./FrameworkListSearchFilter";
-  
+import "./tailwind.css";
+import FrameworkList from "./FrameworkList";
+import FrameworkListSearchFilter from "./FrameworkListSearchFilter";
+import ResponsiveText from "./ResponsiveDesign";
 
 
 createRoot(document.getElementById("root"))
     .render(
-        <div >
-                 {/* <FrameworkList/> */}
-                  <FrameworkSearchFilter/>
-        </div>
+        <div>
+          {/* <FrameworkList /> */}
+          {/* <FrameworkListSearchFilter />     */}
+          <ResponsiveText />
+        </div>      
     )
